de5ecdd7a61f38f82d5d674abb8bf9618b47ae5e
Taira <lftaira@gmail.com>
2020-04-20 17:44:18 +0000

    correcao jpg

--- Controllers/ReceitaController.cs
+++ Controllers/ReceitaController.cs
@@ -101,9 +101,9 @@ namespace ReceitasDeSucesso.Controllers
         {
             string guidImage = null;
             //2097152
-            if (receita.ImagemDaReceita != null || receita.ImagemDaReceita.FileName.ToUpper().EndsWith(".JPEG") ||
+            if (receita.ImagemDaReceita != null && (receita.ImagemDaReceita.FileName.ToUpper().EndsWith(".JPEG") ||
             receita.ImagemDaReceita.FileName.ToUpper().EndsWith(".JPG") || 
-            receita.ImagemDaReceita.FileName.ToUpper().EndsWith(".PNG")) 
+            receita.ImagemDaReceita.FileName.ToUpper().EndsWith(".PNG"))) 
             {
                 var pathImagem = Path.Combine(_webHostEnvironment.WebRootPath, "imgs");
                 
